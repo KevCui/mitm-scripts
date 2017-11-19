ab0c12c69aba509659afd566a95cb18569389da1
Kevin Cui <krazycavin@gmail.com>
2017-11-19 12:48:56 +0100

    Add proper README.md

--- rewrite.py
+++ rewrite.py
@@ -4,50 +4,6 @@ import json
 import yaml
 from mitmproxy import http
 
-# This script is a mitmproxy inline script.
-# The purpose of this script is to return mock JSON response for
-# certain target URLs.
-#
-# PRECONDITION:
-# 1. Requirements installation:
-#   ~$ sudo pip3 install -r requirements.txt
-#
-# 2. Client side CA setup:
-#    http://docs.mitmproxy.org/en/stable/certinstall.html
-#
-# HOW TO USE:
-# 1. Configure global variables:
-#    - HOME_DIR: script directory
-#    - DATA_DIR: JSON files directory
-#    - ROUTER_JSON: router.yaml file directory
-#
-# 2. Run mitmproxy:
-#    ~$ mitmdump -s rewrite.py
-#
-# 3. Quick check setup:
-#    http://example.com/pass should return data in test_pass.json
-#    http://example.com/fail should return data in test_fail.json
-#
-# 4. Update routeerjson, pair URL with JSON file, for e.g:
-#    ```
-#    http://exmaple.com: exmaple
-#    ```
-#    The response of "http://exmaple.com" will be rewrote by the content
-#    in exmaple.json file. Using yaml file is easy for human to read and
-#    it's possible to add comment in yaml.
-#
-# 5. Add static JSON file, file example:
-#    ```
-#    {
-#      "status": 200,
-#      "header": { ... },
-#      "content": ...
-#    }
-#    ```
-#    status:http status code, an INT number
-#    header: http response headers
-#    content: http response body
-
 HOME_DIR = './'
 DATA_DIR = './response/'
 ROUTER_FILE = HOME_DIR + 'router.yaml'
